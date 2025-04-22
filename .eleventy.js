6bc36c806f3bf4821900a3af26bd356743dd3dc1
Adam Alexander <ghedipunk@gmail.com>
2025-04-22 13:20:52 -0600

    Front page and inner pages work.

--- .eleventy.js
+++ .eleventy.js
@@ -51,6 +51,7 @@ module.exports = function (eleventyConfig) {
     eleventyConfig.addPassthroughCopy("./src/assets/images");
     eleventyConfig.addPassthroughCopy("./src/assets/js");
     eleventyConfig.addPassthroughCopy("./src/assets/svgs");
+    eleventyConfig.addPassthroughCopy("./src/admin/config.yml");
 
 
     eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
